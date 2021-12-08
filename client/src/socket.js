58f49590a9ecfdafe4bfa1db07bb64c763f547e8
Maria Helena Norén <marialinder97@hotmail.com>
2021-12-08 16:04:05 +0100

    changed location to heroku

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 
-const socket = io('http://127.0.0.1:4000', {
+const socket = io('https://socket-chatt-app.herokuapp.com/', {
   transports: ['websocket'],
 });
 
